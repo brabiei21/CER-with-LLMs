2f66a78650f501bd4858f377bab2ce202cd6933e
Steve-Morales <steve.morales22001@gmail.com>
2024-05-09 12:59:50 -0700

    Attempting to get specs

--- Code/scraper/main.py
+++ Code/scraper/main.py
@@ -88,6 +88,7 @@ def GetAllProductURLS():
 
         # Move to next page of the product page
         a_tags = driver.find_elements(By.CSS_SELECTOR, "a[class='hd-pagination__link ']")
+        # FIXME: check if it loops arounds
         if a_tags[-1].get_attribute("aria-label") == None or a_tags[-1].get_attribute("aria-label") != "Next":
             REACHED_END = True
         else:
@@ -101,6 +102,75 @@ def GetAllProductURLS():
 
     return links
 
+def GetSpecifications():
+    REACHED_END = False
+    links = None
+    # Check if the file exists
+    if os.path.exists('links.json'):
+        # Read the existing content from the file
+        with open('links.json', 'r') as f:
+            content = f.read()
+            links = json.loads(content)
+        if not (len(content) > 0 and content[-1] != None):
+            print('[ERROR] No links found')
+            return
+    else:
+        print('[ERROR] file not found, exiting function ...')
+        return 
+
+    # Launch a headless Chrome browser
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+
+    for group_of_products in links:
+        for product in group_of_products:
+            link = product[-1]
+            print(link)
+            driver.get(link)
+
+            print("LOADING PAGE...")
+            time.sleep(3)  # Adjust as needed
+            reached_page_end = False
+            last_height = driver.execute_script("return document.body.scrollHeight")
+
+            while not reached_page_end:
+                driver.find_element(By.XPATH, '//body').send_keys(Keys.END)   
+                time.sleep(2)
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if last_height == new_height:
+                        reached_page_end = True
+                else:
+                        last_height = new_height
+            time.sleep(3) # allow remaining page to load
+
+            div_main_specs = driver.find_elements(By.CSS_SELECTOR, "div[class='kpf__specs']")
+            headers = driver.find_elements(By.CSS_SELECTOR, "div[class='kpf__name']")
+            print(headers[0].text)
+            return 
+
+    # Navigate to the webpage
+    print(links[0][0][-1])
+    return
+    driver.get()
+
+    print("LOADING PAGE...")
+    time.sleep(3)  # Adjust as needed
+
+    reached_page_end = False
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while not reached_page_end:
+        driver.find_element(By.XPATH, '//body').send_keys(Keys.END)   
+        time.sleep(2)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if last_height == new_height:
+                reached_page_end = True
+        else:
+                last_height = new_height
+    time.sleep(3) # allow remaining page to load
+
 if __name__ == '__main__':
-    GetAllProductURLS()
+    # GetAllProductURLS()
     # for each link, get specifications
+    GetSpecifications()
